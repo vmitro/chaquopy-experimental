d1cbb9e5cc6e25a4d2337ac87030de3aefb6007c
vmitro <vladimir.mitrovic.de@gmail.com>
2023-05-17 20:09:03 +0000

    chaquopy-libomp: bumped version to 10.1.0 & updated build.sh to use new env vars

--- server/pypi/packages/chaquopy-libomp/build.sh
+++ server/pypi/packages/chaquopy-libomp/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eu
 
-toolchain=$(realpath $(dirname $CC)/..)
-arch=$(echo $CHAQUOPY_TRIPLET | sed 's/-.*//; s/i686/i386/')
+#toolchain=$(realpath $(dirname $CC)/..)
+toolchain=$NDK_TOOLCHAIN
+#arch=$(echo $CHAQUOPY_TRIPLET | sed 's/-.*//; s/i686/i386/')
+arch=$(echo $HOST_TRIPLE | sed 's/-.*//; s/i686/i386/')
+echo $HOST_TRIPLE
+echo $arch
+# /opt/android-sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/lib64/clang/14.0.7/lib/linux/aarch64/
 
 mkdir -p $PREFIX/lib
 cp $toolchain/lib64/clang/$PKG_VERSION/lib/linux/$arch/libomp.so $PREFIX/lib
