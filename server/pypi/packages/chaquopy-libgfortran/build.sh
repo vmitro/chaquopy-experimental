7f8308542847ae2654237e8d773aa8a5a25011a9
vmitro <vladimir.mitrovic.de@gmail.com>
2023-05-17 20:02:29 +0000

    Restored libgfortran again. TODO: fix gfortran build

--- server/pypi/packages/chaquopy-libgfortran/build.sh
+++ server/pypi/packages/chaquopy-libgfortran/build.sh
@@ -3,11 +3,11 @@ set -eu
 
 toolchain=$(realpath $(dirname $FC)/..)
 
-toolchain_version=$($FC --version | head -n1 | sed -E 's/^GNU Fortran \(.*\) ([0-9](\.[0-9])+).*/\1/')
+toolchain_version=$($FC --version | head -n1 | sed -E 's/^GNU Fortran \(.*\) ([0-9]+(\.[0-9])+).*/\1/')
 if [[ $toolchain_version != $PKG_VERSION ]]; then
     echo "Toolchain version '$toolchain_version' doesn't match meta.yaml version '$PKG_VERSION'"
     exit 1
 fi
 
 mkdir -p $PREFIX/lib
-cp $(dirname $CC)/../*/lib*/libgfortran.so.3 $PREFIX/lib
+cp /usr/lib*/*/libgfortran.so.5 $PREFIX/lib
