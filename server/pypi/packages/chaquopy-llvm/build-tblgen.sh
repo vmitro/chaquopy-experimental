0e8f0d86bb3ceed5ff013e5d989ec4ad9b93eba4
vmitro <vladimir.mitrovic.de@gmail.com>
2023-05-17 19:26:21 +0000

    chaquopy-llvm: bumped version to 11 according to numba's requirements

--- server/pypi/packages/chaquopy-llvm/build-tblgen.sh
+++ server/pypi/packages/chaquopy-llvm/build-tblgen.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -eu
 
+echo ">>>>>> NOW IN BUILD-TBLGEN.SH"
+
+#echo ">>>>>> before unset"
+#export
+
 build_dir=$1
+
 mkdir -p $build_dir
 cd $build_dir
 
 unset AR ARFLAGS AS CC CFLAGS CPP CPPFLAGS CXX CXXFLAGS F77 F90 FARCH FC LD LDFLAGS LDSHARED \
-      NM RANLIB READELF STRIP
+      NM RANLIB READELF STRIP CMAKE_TOOLCHAIN_FILE
 
-cmake -G Ninja ..
+cmake -G 'Unix Makefiles' ..
 cmake --build . --target llvm-tblgen -- -j $(nproc)
--- server/pypi/packages/chaquopy-llvm/build.sh
+++ server/pypi/packages/chaquopy-llvm/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
 set -eu
 
 # Based on https://github.com/numba/llvmlite/tree/master/conda-recipes/llvmdev, which,
 # according to https://llvmlite.readthedocs.io/en/latest/admin-guide/install.html, "is the
 # canonical reference for building LLVM for llvmlite".
 
+# build only these targets
+#LLVM_TARGETS_TO_BUILD=${LLVM_TARGETS_TO_BUILD:-"host;"}
+
 # There are undefined symbols in plugin modules, e.g lib/Transforms/Hello.
 LDFLAGS=$(echo $LDFLAGS | sed 's/-Wl,--no-undefined//')
 
-triple=$(basename $AR | sed 's/-ar$//')
+triple=$(basename $FAUX_AR | sed 's/-ar$//')
 case $triple in
     arm-linux-androideabi)
         target="ARM"
@@ -27,15 +31,46 @@ case $triple in
         exit 1
 esac
 
-build_tblgen=$(realpath build-tblgen)
-if [ ! -e $build_tblgen ]; then  # For rerunning with build-wheel.py --no-unpack.
-    $RECIPE_DIR/build-tblgen.sh $build_tblgen
+# add our target to the list of targets to build
+LLVM_TARGETS_TO_BUILD=$target
+echo " >>>> LLVM_TARGETS_TO_BUILD=$LLVM_TARGETS_TO_BUILD"
+
+# renaming the llvm-{{version}} for easier access later (only for llvm 14.0.6)
+[ -d llvm-*.src ] && mv llvm-*.src/ llvm
+
+# we must build the native llvm-config in order to process theneeded libs
+build_llmvconfig=$(realpath build-llmvconfig)
+if [ ! -e $build_llmvconfig ]; then
+    echo "Building llmv-config in $build_llmvconfig..."
+    export triple
+    _TARGET=$target $RECIPE_DIR/build-llmvconfig.sh $build_llmvconfig
+    echo ">>>> BACK IN BUILD.SH"
 fi
 
+build_tblgen=$(realpath build-tblgen)
+if [ ! -e $build_tblgen ]; then  # For rerunning with build-wheel.py --no-unpack.
+    # preserve current path: tblgen build needs a fresh c compiler; build-wheel.py sets the PATH env variable to include chaquopy
+    # and ndk bin directories
+    export TEMP_PATH=$PATH
+    export PATH=$OLD_PATH
+    $RECIPE_DIR/build-tblgen.sh $build_tblgen
+    export PATH=$TEMP_PATH
+fi
+
+#export LDFLAGS+=" $(/opt/android-sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-config --libs --ldflags)"
+#export CFLAGS+=" $(/opt/android-sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-config --cflags)"
+#echo "LDFLAGS=$LDFLAGS"
+#echo "CFLAGS=$CFLAGS"
+
 declare -a _cmake_config
+_cmake_config+=(-DCMAKE_SYSTEM_NAME=Android)
 _cmake_config+=(-DCMAKE_INSTALL_PREFIX:PATH=${PREFIX})
 _cmake_config+=(-DCMAKE_BUILD_TYPE:STRING=Release)
 _cmake_config+=(-DLLVM_BUILD_LLVM_DYLIB=ON)
+#_cmake_config+=(-DLLVM_LINK_LLVM_DYLIB=ON)
+# these two must be set to 'hidden' in order to successfully link libLLVM.so
+_cmake_config+=(-DCMAKE_C_VISIBILITY_PRESET=hidden)
+_cmake_config+=(-DCMAKE_CXX_VISIBILITY_PRESET=hidden)
 _cmake_config+=(-DLLVM_TABLEGEN=$(realpath $build_tblgen/bin/llvm-tblgen))
 
 _cmake_config+=(-DCMAKE_TOOLCHAIN_FILE="../chaquopy.toolchain.cmake")
@@ -65,19 +100,34 @@ _cmake_config+=(-DLLVM_INCLUDE_BENCHMARKS=OFF)
 _cmake_config+=(-DLLVM_INCLUDE_DOCS=OFF)
 _cmake_config+=(-DLLVM_INCLUDE_EXAMPLES=OFF)
 _cmake_config+=(-DLLVM_INCLUDE_TESTS=OFF)
+_cmake_config+=(-DLLVM_DYLIB_COMPONENTS=all)
+#_cmake_config+=(-DCMAKE_CROSSCOMPILING=True) # it compiled successfully with this setting off
 
-mkdir -p build
+# For rerunning with build-wheel.py --no-unpack.
+[ ! -d build ] && mkdir -p build
+
 cd build
 rm -f CMakeCache.txt  # For rerunning with build-wheel.py --no-unpack.
+#cmake -G 'Unix Makefiles'     \
+cmake "${_cmake_config[@]}"  \
+      ../
 
-cmake -G Ninja "${_cmake_config[@]}" ..
-cmake --build . --target install -- -j $(nproc)
+cmake --build . -j 8
+cmake --build . --target install -- -j  8 
+# we create a tar archive containing the files needed for native llmv-config
+#cd ../build-llmvconfig
+#tar -cvf $PREFIX/llvm-config.tar .
 
-cd $PREFIX
-rm -r bin share
+# UNCOMMENT THE FOLLOWING LINES
+#cp -r $build_llmvconfig/* $PREFIX
+#chmod -x $PREFIX/bin/llvm-config
+
+#echo PREFIX=$PREFIX
+#cd $PREFIX
+rm -r bin                                          # CAN UNCOMMENT; keeping those just for llvmlite to compile successfully
 cd $PREFIX/lib
-rm -r *.a cmake
-find -type l | xargs rm
-for name in *.so; do
-    if [[ ! $name =~ ^libLLVM- ]]; then rm $name; fi
-done
+rm -r cmake                                         # CAN UNCOMMENT
+find -type l | xargs -r rm                              # CAN UNCOMMENT
+#for name in *.so; do
+#    if [[ ! $name =~ ^libLLVM- ]]; then rm $name; fi    # CAN UNCOMMENT
+#done
