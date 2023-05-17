b18310c63e371fdc12e2db686f552640da11de68
vmitro <vladimir.mitrovic.de@gmail.com>
2023-05-17 19:43:18 +0000

    Modified chaquopy-libcxx: use new env vars

--- server/pypi/packages/chaquopy-libcxx/build.sh
+++ server/pypi/packages/chaquopy-libcxx/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eu
 
-toolchain=$(realpath $(dirname $CC)/..)
+#toolchain=$(realpath $(dirname $CC)/..)
+toolchain=$NDK_TOOLCHAIN
 
 header_version=$(cat $toolchain/sysroot/usr/include/c++/v1/__libcpp_version)
+echo "header_version=$header_version"
 if [[ $header_version != $PKG_VERSION ]]; then
     echo "Header version '$header_version' doesn't match meta.yaml version '$PKG_VERSION'"
     exit 1
 fi
 
 mkdir -p $PREFIX/lib
-cp $toolchain/sysroot/usr/lib/$CHAQUOPY_TRIPLET/libc++_shared.so $PREFIX/lib
+cp $toolchain/sysroot/usr/lib/$HOST_TRIPLE/libc++_shared.so $PREFIX/lib
